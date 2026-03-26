03beb6f632e281e70f098c9f4ad62e38eed8378c
Bebsiizcool <basilshahzad18@gmail.com>
2026-03-26 15:34:42 +0500

    useState and hooks

--- ts-tsender/my-app/src/components/Airdropform.tsx
+++ ts-tsender/my-app/src/components/Airdropform.tsx
@@ -2,10 +2,20 @@
 import InputField from "./ui/InputField"
 import { useState } from "react";
 
-export default function AirDropform(){
-    const [tokenaddress, settokkenaddress] = useState("");
-    return(
-            <div className="p-6 space-y-4">
+export default function AirDropform() {
+  const [tokenaddress, settokkenaddress] = useState("");
+  const [Recipients, setrecipients] = useState("")
+  const [Amount, setamount] = useState("")
+
+  async function handlesubmit(){
+    console.log(tokenaddress)
+    console.log(Recipients)
+    console.log(Amount)
+  }
+
+  return (
+    <div className="p-6 space-y-4">
+
       <InputField
         label="Tokenaddress"
         placeholder="0x..."
@@ -13,7 +23,26 @@ export default function AirDropform(){
         onChange={(e) => settokkenaddress(e.target.value)}
       />
 
+      <InputField
+        label="Recipients"
+        placeholder="0x1234,0x1246..."
+        value={Recipients}
+        onChange={(e) => setrecipients(e.target.value)}
+        large = {true}
+      />
+
+      <InputField
+        label="Amount"
+        placeholder="100, 200, 300...."
+        value={Amount}
+        onChange={(e) => setamount(e.target.value)}
+        large = {true}
+      />
+
+      <button onClick={handlesubmit}>
+        send tokens
+      </button>
     </div>
   );
 }
-    
+
