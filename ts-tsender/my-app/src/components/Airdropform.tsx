ad46e5d15e01db7c718ca41dc9549d2c3bf6359d
Bebsiizcool <basilshahzad18@gmail.com>
2026-03-29 21:29:02 +0500

    update

--- ts-tsender/my-app/src/components/Airdropform.tsx
+++ ts-tsender/my-app/src/components/Airdropform.tsx
@@ -2,8 +2,8 @@
 import InputField from "./ui/InputField"
 import { useState, useMemo } from "react";
 import { chainsToTSender, tsenderAbi, erc20Abi } from "@/constants";
-import {useChainId, useConfig, useAccount, useWriteContract} from 'wagmi'
-import {readContract, waitForTransactionReceipt} from '@wagmi/core'
+import { useChainId, useConfig, useAccount, useWriteContract } from 'wagmi'
+import { readContract, waitForTransactionReceipt } from '@wagmi/core'
 import { calculateTotal } from "./utils/calculateTotal";
 
 
@@ -14,30 +14,30 @@ export default function AirDropform() {
   const chainId = useChainId()
   const config = useConfig()
   const account = useAccount()
-  const total: number = useMemo(() => calculateTotal(Amount), [Amount] );
-  const {data: hash, isPending, writeContractAsync} = useWriteContract()
+  const total: number = useMemo(() => calculateTotal(Amount), [Amount]);
+  const { data: hash, isPending, writeContractAsync } = useWriteContract()
 
-  async function getapproved(tsenderAddress: string) : Promise<number>{
-    if(!tsenderAddress){
+  async function getapproved(tsenderAddress: string): Promise<number> {
+    if (!tsenderAddress) {
       alert("No address found, please use a supported chain")
       return 0
     }
 
-    const response = await readContract(config,{
+    const response = await readContract(config, {
       abi: erc20Abi,
       address: tokenaddress as `0x${string}`,
       functionName: "allowance",
       args: [account.address, tsenderAddress as `0x${string}`],
 
     })
-    
+
     return response as number
   }
 
-  async function handlesubmit(){
-    const tsenderAddress = chainsToTSender[chainId] ["tsender"]
+  async function handlesubmit() {
+    const tsenderAddress = chainsToTSender[chainId]["tsender"]
     const approvedamount = await getapproved(tsenderAddress)
-    if(approvedamount < total){
+    if (approvedamount < total) {
       const approvalHash = await writeContractAsync({
         abi: erc20Abi,
         address: tokenaddress as `0x${string}`,
@@ -45,10 +45,36 @@ export default function AirDropform() {
         args: [tsenderAddress as `0x${string}`, BigInt(total)],
       })
 
-      const approvalRecipt = await waitForTransactionReceipt(config,{
-      hash: approvalHash
-    })
-    console.log("Approval confirmed", approvalRecipt)
+      const approvalRecipt = await waitForTransactionReceipt(config, {
+        hash: approvalHash
+      })
+      console.log("Approval confirmed", approvalRecipt)
+      await writeContractAsync({
+        abi: tsenderAbi,
+        address: tokenaddress as `0x${string}`,
+        functionName: "airdropERC20",
+        args: [
+          tokenaddress,
+          Recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+          Amount.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+          BigInt(total),
+        ]
+      })
+
+
+    }
+    else {
+      await writeContractAsync({
+        abi: tsenderAbi,
+        address: tokenaddress as `0x${string}`,
+        functionName: "airdropERC20",
+        args: [
+          tokenaddress,
+          Recipients.split(/[,\n]+/).map(addr => addr.trim()).filter(addr => addr !== ''),
+          Amount.split(/[,\n]+/).map(amt => amt.trim()).filter(amt => amt !== ''),
+          BigInt(total),
+        ]
+      })
     }
   }
 
@@ -67,7 +93,7 @@ export default function AirDropform() {
         placeholder="0x1234,0x1246..."
         value={Recipients}
         onChange={(e) => setrecipients(e.target.value)}
-        large = {true}
+        large={true}
       />
 
       <InputField
@@ -75,10 +101,10 @@ export default function AirDropform() {
         placeholder="100, 200, 300...."
         value={Amount}
         onChange={(e) => setamount(e.target.value)}
-        large = {true}
+        large={true}
       />
 
-      <button onClick={handlesubmit}  className="px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-md hover:from-blue-600 hover:to-indigo-700 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
+      <button onClick={handlesubmit} className="px-6 py-3 bg-gradient-to-r from-blue-500 to-indigo-600 text-white font-semibold rounded-lg shadow-md hover:from-blue-600 hover:to-indigo-700 transition-all duration-200 focus:outline-none focus:ring-2 focus:ring-blue-400 focus:ring-opacity-75">
         send tokens
       </button>
     </div>
--- ts-tsender/my-app/src/rainbowkitconfig.tsx
+++ ts-tsender/my-app/src/rainbowkitconfig.tsx
@@ -2,12 +2,12 @@
 
 "use client"  
 import { getDefaultConfig } from "@rainbow-me/rainbowkit"
-import {anvil, zksync, mainnet} from "wagmi/chains"
+import {anvil, zksync, mainnet, sepolia} from "wagmi/chains"
 
 export default getDefaultConfig({
     appName: "my-app",
     // projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID!,
     projectId: "da080012497d6db7f2624e1091de2409",
-    chains: [anvil, zksync, mainnet],
+    chains: [anvil, zksync, mainnet, sepolia],
     ssr: false
 })
